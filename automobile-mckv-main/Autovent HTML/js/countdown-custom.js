9d21fc468f18560109e662f12082666d217cd26d
Zainab Nadeem Fazaa Alshubati <zainab.nadeem2015@gmail.com>
2023-12-06 10:21:11 +0300

    @zainab nadeem

---
+++ automobile-mckv-main/Autovent HTML/js/countdown-custom.js
@@ -0,0 +1,6 @@
+jQuery(document).ready(function() {
+        jQuery(function () {
+            jQuery('#defaultCountdown').countdown({until: new Date(2023, 10, 10, 2)}); // year, month, date, hour
+        });
+});		
+
